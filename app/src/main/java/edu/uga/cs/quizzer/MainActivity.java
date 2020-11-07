ccec40b9126f40000249a94829594606427203d5
valekll <lordvalekllofvalour@gmail.com>
2020-11-07 15:52:51 -0500

    Activities Added
    Quiz and Results activities added and linked to buttons.

--- app/src/main/java/edu/uga/cs/quizzer/MainActivity.java
+++ app/src/main/java/edu/uga/cs/quizzer/MainActivity.java
@@ -2,13 +2,36 @@ package edu.uga.cs.quizzer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
+    private Button resultsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button)findViewById(R.id.startQuizButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent quizIntent = new Intent(view.getContext(), QuizActivity.class);
+                MainActivity.this.startActivity(quizIntent);
+            }
+        });
+        resultsButton = (Button)findViewById(R.id.resultsButton);
+        resultsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultsIntent = new Intent(view.getContext(), ResultsActivity.class);
+                MainActivity.this.startActivity(resultsIntent);
+            }
+        });
     }
 }
