9de31fcd6ca0c916ba171bad7f52944c0ae9d9e6
valekll <lordvalekllofvalour@gmail.com>
2020-11-10 02:33:16 -0500

    Update EndQuizFragment.java
    Comments added

--- app/src/main/java/edu/uga/cs/quizzer/EndQuizFragment.java
+++ app/src/main/java/edu/uga/cs/quizzer/EndQuizFragment.java
@@ -11,24 +11,36 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 /**
- * A simple {@link Fragment} subclass.
+ * A simple fragment to complete the quiz
  */
 public class EndQuizFragment extends Fragment {
 
+    // Required empty public constructor
     public EndQuizFragment() {
-        // Required empty public constructor
     }
 
+    /**
+     * Creates the Fragment based on savedInstanceState
+     * @param savedInstanceState the state the fragment should be created in
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * Creates the view for the fragment
+     * @param inflater a view inflater for the xml
+     * @param container the container in the view
+     * @param savedInstanceState the state in which the fragment is being created.
+     * @return the view for display
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_end_quiz, container, false);
+        // Create button to submit quiz
         Button submit = (Button)rootView.findViewById(R.id.submitButton);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
