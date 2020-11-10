e913977818d7b25c37eab1e6cbbaa63f71f8ac2f
valekll <lordvalekllofvalour@gmail.com>
2020-11-10 02:49:04 -0500

    Update InformationActivity.java
    Added comments

--- app/src/main/java/edu/uga/cs/quizzer/InformationActivity.java
+++ app/src/main/java/edu/uga/cs/quizzer/InformationActivity.java
@@ -5,23 +5,26 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Information activity is a simple activity that displays how to use the app.
+ */
 public class InformationActivity extends AppCompatActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_information);
 
-            TextView textView = findViewById(R.id.textView1);
 
-            textView.setText(
-                    "How to play the game: \n\n" +
-                        "To start, please select on the start button \n\n" +
-                        "The app will now prompt the user to answer \n\n" +
-                        "Swipe left when the question has been answered (total of six questions) \n\n" +
-                        "After all six questions have been answered, your results will be shown\n\n\n\n" +
-                    "Additional Information: \n\n" +
-                            "To view past result scores, please select on the results button\n\n"
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
 
+        //Get reference to textbox and set it to show rules.
+        TextView textView = findViewById(R.id.textView1);
+        textView.setText(
+                "How to play the game: \n\n" + "To start, please select on the start button \n\n" +
+                "The app will now prompt the user to answer \n\n" +
+                "Swipe left when the question has been answered (total of six questions) \n\n" +
+                "After all six questions have been answered, your results will be shown\n\n\n\n" +
+                "Additional Information: \n\n" +
+                "To view past result scores, please select on the results button\n\n"
             );
-        }
+    }
 }
