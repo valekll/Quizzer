0ebbc3df145feb56ce1f10801f98cf4c459652ac
HR47779 <55899689+HR47779@users.noreply.github.com>
2020-11-09 23:15:35 -0500

    updated number of pages.
    App will not crash when we get to the sixth page.

--- app/src/main/java/edu/uga/cs/quizzer/ScreenSlidePagerActivity.java
+++ app/src/main/java/edu/uga/cs/quizzer/ScreenSlidePagerActivity.java
@@ -26,7 +26,7 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
     /**
      * The number of pages (wizard steps) to show in this demo.
      */
-    private static final int NUM_PAGES = 7;
+    private static final int NUM_PAGES = 6;
 
     /**
      * The pager widget, which handles animation and allows swiping horizontally to access previous
